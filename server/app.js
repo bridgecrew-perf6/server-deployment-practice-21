a83f136b28093c0f736a67a230c43bcd8ed05a36
Jeffrey Jenkins <jeffreyjtech@gmail.com>
2022-03-21 21:42:56 -0700

    Added data route handler

--- server/app.js
+++ server/app.js
@@ -8,4 +8,8 @@ app.get('/hello', (req, res, next) => {
   res.send('Hello world\nOwO');
 });
 
+app.get('/data', (req, res, next) => {
+  res.send('Welcome the data route my dude.');
+});
+
 module.exports = app;
